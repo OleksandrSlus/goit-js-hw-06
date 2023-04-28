8eb21ab603038b5197b0903d6b7aa158dde484c2
Oleksandr <121134310+OleksandrSlus@users.noreply.github.com>
2023-04-28 20:00:03 +0300

    zaruba 0.2 add 6789

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,17 @@
-
+const rightEmail = "";
+const rightPassword = "";
+const loginForm = document.querySelector("loginForm");
+loginForm.addEventListener("submit", onFormSubmit);
+function onFormSubmit(event) {
+    event.preventDefault();
+    const emailValue = formElements.email.value;
+    const passwordValue = formElements.password.value;
+    if (emailValue == rightEmail && passwordValue == rightPassword ) {
+      return console.log ({
+        elementsEmail: rightEmail,
+        elementsPassword: rightPassword,
+      });
+    } else if (emailValue == "" || passwordValue == "") {
+        alert("Fill up all the forms")
+    }
+}
--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,11 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const changeColorEl = document.querySelector(".change-color");
+const textColorEl = document.querySelector(".color");
+changeColorEl.addEventListener("click", onChangeColor);
+function onChangeColor() {
+  const color = getRandomHexColor();
+  textColorEl.textContent = color;
+  body.style.backgroundColor = color;
+}
